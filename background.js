76310492c38868d2532ad4b9a94386bff6b30452
Mannu021 <mayanksaini021@gmail.com>
2022-01-29 17:27:38 +0530

    remove the on sign from badge on reload

--- background.js
+++ background.js
@@ -8,13 +8,16 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
         chrome.scripting.executeScript({
             target: { tabId: tabId },
-            files: ["./foreground.js"]})
+            files: ["./foreground.js"]}),
+        chrome.action.setBadgeText({text: ''});
+    
     }
-       
+    
 });
 
+
 chrome.alarms.onAlarm.addListener(async function() {
-    chrome.action.setBadgeText({text: 'yes'}),
+    chrome.action.setBadgeText({text: ''}),
     chrome.scripting.insertCSS({
             target: { tabId: (await getCurrentTab()).id },
             files: ["inject.css"]
